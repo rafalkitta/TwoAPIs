6cd83baa8bbc6b864323723d60e6090a6f47fd61
Rafał Kitta <rafalkit@gmail.com>
2020-08-09 22:16:27 +0200

    Adds simple collection view with flow layout

--- TwoAPIs/ViewController.swift
+++ TwoAPIs/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ItemsViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView! {
+        didSet {
+            collectionView.dataSource = self
+        }
+    }
+    @IBOutlet weak var collectionViewFlowLayout: UICollectionViewFlowLayout!
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        updateItemSize()
+    }
+    
+    private func updateItemSize() {
+        let cellsPerRow = 2
+        let safeAreaInsets: UIEdgeInsets
+        if #available(iOS 11.0, *) {
+            safeAreaInsets = view.safeAreaInsets
+        } else {
+            safeAreaInsets = .zero
+        }
+        let marginsAndInsets = collectionViewFlowLayout.sectionInset.left +
+            collectionViewFlowLayout.sectionInset.right +
+            safeAreaInsets.left + safeAreaInsets.right +
+            collectionViewFlowLayout.minimumInteritemSpacing * CGFloat(cellsPerRow - 1)
+        let itemWidth = ((view.bounds.size.width - marginsAndInsets) / CGFloat(cellsPerRow)).rounded(.down)
+        collectionViewFlowLayout.itemSize =  CGSize(width: itemWidth, height: itemWidth)
     }
 }
 
+// MARK: - UICollectionViewDataSource
+extension ItemsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCell", for: indexPath)
+        return cell
+    }
+}
